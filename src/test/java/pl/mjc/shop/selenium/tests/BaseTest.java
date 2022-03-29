e120ab8134e9fdeed44f490357d6cd3584ea3849
Marcin CIESIELSKI <marcin.jan.ciesielski@gmail.com>
2022-03-29 13:09:04 +0200

    Add BaseTest.

---
+++ src/test/java/pl/mjc/shop/selenium/tests/BaseTest.java
@@ -0,0 +1,25 @@
+package pl.mjc.shop.selenium.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pl.mjc.shop.selenium.utils.DriverFactory;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+    
+    @BeforeMethod
+    public void beforeMethode(){
+        driver = DriverFactory.getDriver("firefox");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+    
+}
