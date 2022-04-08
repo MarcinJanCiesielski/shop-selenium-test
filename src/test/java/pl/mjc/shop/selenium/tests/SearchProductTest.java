f7dea58df8c37820b888270659a537539f9f5335
Marcin CIESIELSKI <marcin.jan.ciesielski@gmail.com>
2022-04-08 15:41:31 +0200

    Move openPage to BaseTest. Add test for product search.

--- src/test/java/pl/mjc/shop/selenium/tests/SearchProductTest.java
+++ src/test/java/pl/mjc/shop/selenium/tests/SearchProductTest.java
@@ -1,10 +1,30 @@
 package pl.mjc.shop.selenium.tests;
 
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import pl.mjc.shop.selenium.pages.HomePage;
+import pl.mjc.shop.selenium.pages.SearchProductPage;
+import pl.mjc.shop.selenium.pages.SearchProductResultsPage;
+
+import java.util.List;
 
 public class SearchProductTest extends BaseTest {
     @Test
-    public void openPage() {
-        driver.get("http://seleniumdemo.com/");
+    public void searchProductTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickSearchButton();
+        
+        SearchProductPage searchProductPage = new SearchProductPage(driver);
+        searchProductPage.performSearch("git");
+        
+        SearchProductResultsPage resultsPage = new SearchProductResultsPage(driver);
+        List<String> products = resultsPage.getProducts()
+                                           .stream()
+                                            .filter(WebElement::isDisplayed)
+                                           .map(WebElement::getText)
+                                           .toList();
+        for(String product : products) {
+            System.out.println("PROD: " + product);
+        }
     }
 }
