a75b046308bf88dcec04ce2222b157f6886083fa
Marcin CIESIELSKI <marcin.jan.ciesielski@gmail.com>
2022-04-08 15:37:58 +0200

    Add methode to perform search.

--- src/test/java/pl/mjc/shop/selenium/pages/SearchProductPage.java
+++ src/test/java/pl/mjc/shop/selenium/pages/SearchProductPage.java
@@ -1,4 +1,26 @@
 package pl.mjc.shop.selenium.pages;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class SearchProductPage {
+    @FindBy(name = "s")
+    List<WebElement> searchInput;
+    
+    public SearchProductPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+    
+    public void performSearch(String search) {
+        searchInput
+            .stream()
+                .filter(WebElement::isDisplayed)
+                    .findFirst()
+                .ifPresent(el -> el.sendKeys(search + Keys.ENTER));
+    }
 }
