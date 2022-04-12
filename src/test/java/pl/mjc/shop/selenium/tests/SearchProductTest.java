2876250803d146e77f98465a1a7de6aa007dccfa
Marcin CIESIELSKI <marcin.jan.ciesielski@gmail.com>
2022-04-12 16:16:55 +0200

    Change searchProductTest methode to use DataProvider. Add DataProvider. Add file SearchProductData.xlsx with data for test.

--- src/test/java/pl/mjc/shop/selenium/tests/SearchProductTest.java
+++ src/test/java/pl/mjc/shop/selenium/tests/SearchProductTest.java
@@ -2,21 +2,24 @@ package pl.mjc.shop.selenium.tests;
 
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.mjc.shop.selenium.pages.HomePage;
 import pl.mjc.shop.selenium.pages.SearchProductPage;
 import pl.mjc.shop.selenium.pages.SearchProductResultsPage;
+import pl.mjc.shop.selenium.utils.ExcelRead;
 
+import java.io.IOException;
 import java.util.List;
 
 public class SearchProductTest extends BaseTest {
-    @Test
-    public void searchProductTest() {
+    @Test(dataProvider = "getSearchProductData")
+    public void searchProductTest(String search, String results) {
         HomePage homePage = new HomePage(driver);
         homePage.clickSearchButton();
         
         SearchProductPage searchProductPage = new SearchProductPage(driver);
-        searchProductPage.performSearch("git");
+        searchProductPage.performSearch(search);
         
         SearchProductResultsPage resultsPage = new SearchProductResultsPage(driver);
         try {
@@ -27,9 +30,14 @@ public class SearchProductTest extends BaseTest {
         }
         List<String> products = resultsPage.getProducts()
                                            .stream()
-                                            .filter(WebElement::isDisplayed)
+                                           .filter(WebElement::isDisplayed)
                                            .map(WebElement::getText)
                                            .toList();
-        Assert.assertTrue(products.contains("GIT basics"));
+        Assert.assertTrue(products.contains(results));
+    }
+    
+    @DataProvider
+    public Object[][] getSearchProductData() throws IOException {
+        return ExcelRead.readExcelData("SearchProductData.xlsx");
     }
 }
